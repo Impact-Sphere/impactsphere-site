9cce593f0a3adcb8684d3fffcc98be0fe53337e6
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2026-03-24 18:29:07 +0000

    removed temp code

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
     <div>
       <h1 className="flex justify-center items-center align-middle">
         IMPACT SPHERE WILL BE HERE SOON (i hope)
-        <p>If you can see this text, it means the auto-deployment is working</p>
       </h1>
     </div>
   );
