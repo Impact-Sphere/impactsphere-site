b5bb9649965f35fd83bb45f507ddcefdde639432
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2026-03-24 18:25:48 +0000

    readded temp code

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div>
       <h1 className="flex justify-center items-center align-middle">
         IMPACT SPHERE WILL BE HERE SOON (i hope)
+        <p>If you can see this text, it means the auto-deployment is working</p>
       </h1>
     </div>
   );
